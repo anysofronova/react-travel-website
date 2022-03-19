6df79dbe082d372f490147d1bb3a3eb4945a799d
Anna <anysofronova@yandex.ru>
2022-03-19 18:52:12 +0300

    Footer is added

--- react-travel-website/src/components/HeroSection.jsx
+++ react-travel-website/src/components/HeroSection.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./HeroSection.scss";
 import "../App.scss";
 import { Button } from "./Button";
-import natureVideo from "../video/Nature.mp4";
+// import natureVideo from "../video/Nature.mp4";
 import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function HeroSection() {
   return (
     <div className="hero__container">
-      <video src={natureVideo} autoPlay loop muted />
+      {/* <video src={natureVideo} autoPlay loop muted /> */}
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className="hero__buttons">
